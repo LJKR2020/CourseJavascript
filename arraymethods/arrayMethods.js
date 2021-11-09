04ad15cb80a16d7d3b84d8744bfe701e06336498
LJKR2020 <loredana.rietveld@gmail.com>
2021-11-09 13:39:17 +0100

    compared answers with teacher

--- arraymethods/arrayMethods.js
+++ arraymethods/arrayMethods.js
@@ -19,19 +19,25 @@ const inventors = [
 // Verwachte uitkomst:
 // [{ first: 'Galileo', last: 'Galilei', year: 1564, passed: 1642 }, { first: 'Johannes', last: 'Kepler', year: 1571, passed: 1630 }]
 
-const inventors16 = inventors.filter((inventor) => {return inventor.year >= 1500 && inventor.year < 1600});
+const inventors16 = inventors.filter((inventor) => {
+    return inventor.year >= 1500 && inventor.year < 1600
+});
 console.log(inventors16);
 
 // 2. Maak een array met daarin alle geboortejaren van de uitvinders
 // Verwachte uitkomst: [1879, 1643, 1564, 1867, 1571, 1473, 1858, 1898, 1815, 1855, 1878, 1847];
 
-const inventorsByYear = inventors.map((inventor) => {return inventor.year});
+const inventorsByYear = inventors.map((inventor) => {
+    return inventor.year
+});
 console.log(inventorsByYear);
 
 // 3. Maak een array met daarin alle volledige namen van de uitvinders (dus voor- en achternaam als één string)
 // Verwachte uitkomst: [ 'Albert Einstein', 'Isaac Newton', 'Galileo Galilei', 'Marie Curie', 'Johannes Kepler', 'Nicolaus Copernicus', 'Max Planck', 'Katherine Blodgett', 'Ada Lovelace', 'Sarah E. Goode', 'Lise Meitner', 'Thomas Edison']
 
-const inventorsFullname = inventors.map((inventor) => {return inventor.first + " " + inventor.last});
+const inventorsFullname = inventors.map((inventor) => {
+    return inventor.first + " " + inventor.last
+});
 console.log(inventorsFullname);
 
 // 4. Sorteer de uitvinders op geboortejaar, oplopend van oudste naar jongste uitvinder
@@ -82,5 +88,7 @@ console.log(inventors);
 // 6. Vind de gegevens over de uitvinder wiens achternaam 'Edison' is.
 // Verwachte uitkomst: { first: 'Thomas', last: 'Edison', year: 1847, passed: 1931 }
 
-const specific = inventors.find((inventor) => {return inventor.last === "Edison"});
+const specific = inventors.find((inventor) => {
+    return inventor.last === "Edison"
+});
 console.log(specific);
