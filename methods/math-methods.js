b22d3a998e15e3c0587d0716566b3eb6d121236d
LJKR2020 <loredana.rietveld@gmail.com>
2022-01-06 12:32:39 +0100

    syntax error math to Math fixed

--- methods/math-methods.js
+++ methods/math-methods.js
@@ -5,26 +5,56 @@
 console.log(Math.PI);
 
 /* Opdracht 1: */
-// Bereken de omtrek van een cirkel met een diameter van 4 en log dit in de terminal. De eenheid (cm, meters, etc. maakt niet uit).
+// Bereken de omtrek van een cirkel met een diameter van 4 en log dit in
+// de terminal. De eenheid (cm, meters, etc. maakt niet uit).
 // Tip: de formule is omtrek = diameter * pi
 // ---- Verwachte uitkomst: 12.566370614359172
 
+let diameter = 4;
+let circle = diameter * Math.PI;
+
+console.log(circle);
+
 
 /* Opdracht 2: */
-// Bereken de omtrek van een cirkel met een diameter van 12 en rond dit af naar een heel getal. Log dit in de terminal.
+// Bereken de omtrek van een cirkel met een diameter van 12 en rond dit af naar
+// een heel getal. Log dit in de terminal.
 // ---- Verwachte uitkomst: 38
 
+diameter = 12;
+circle = diameter * Math.PI;
+console.log(Math.round(circle));
 
+//teacher
+const roundedOmtrek = Math.round(12 * Math.PI);
+console.log(roundedOmtrek);
 
 /* Opdracht 3: */
 // Bereken de wortel van 9 en log dit in de terminal
 // ---- Verwachte uitkomst: 3
 
+console.log(Math.sqrt(9));
 
+//teacher
+const root = Math.sqrt(9);
+console.log(root);
 
 /* Opdracht 4: */
 // Genereer een willekeurig getal tussen de 0 en 1 en doe dan een check:
-// * is het willekeurige getal groter dan 0.5 ? Log dan het getal met "Het glas is halfvol" in de terminal
-// * is het willekeurige getal kleiner dan 0.5 ? Log dan het getal met "Het glas is half leeg" in de terminal
-// Tip: iedere keer als je het script opnieuw draait (wel even een extra spatie toevoegen o.i.d.) is dit getal anders ;)
+// * is het willekeurige getal groter dan 0.5 ? Log dan het getal met
+// "Het glas is halfvol" in de terminal
+// * is het willekeurige getal kleiner dan 0.5 ? Log dan het getal met
+// "Het glas is half leeg" in de terminal
+// Tip: iedere keer als je het script opnieuw draait (wel even een extra
+// spatie toevoegen o.i.d.) is dit getal anders ;)
 // ---- Verwachte uitkomst: Bijbehorende zin afhankelijk van de uitkomst
+
+const number = Math.random();
+if (number > 0.5) {
+    console.log('Het glas is halfvol');
+}
+if (number < 0.5) {
+    console.log('Het glas is half leeg');
+}
+
+// the rest of my answers are the same with the answer sheet.
